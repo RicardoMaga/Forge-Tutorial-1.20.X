c61f4bb24e29f8389fd3da59e2c7ca32209308c1
Ricardo <ricardontmagalhaes04@gmail.com>
2024-05-24 21:35:03 +0100

    Ores, loot tables, furnaces.

--- src/main/java/net/ricardo/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/ricardo/tutorialmod/block/ModBlocks.java
@@ -1,12 +1,10 @@
 package net.ricardo.tutorialmod.block;
 
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.PumpkinBlock;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,10 +16,26 @@ import net.ricardo.tutorialmod.item.ModItems;
 import javax.swing.plaf.PanelUI;
 import java.util.function.Supplier;
 
+
+//registar um bloco novo
 public class ModBlocks {
        public static final DeferredRegister<Block> BLOCKS =
                DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
+       public static final RegistryObject<Block> SAPPHIRE_ORE = registerBlock("sapphire_ore",
+             () -> new DropExperienceBlock( BlockBehaviour.Properties.copy(Blocks.STONE)
+                       .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(10,11)));
+       public static final RegistryObject<Block> DEEPSLATE_SAPPHIRE_ORE = registerBlock("deepslate_sapphire_ore",
+            () -> new DropExperienceBlock( BlockBehaviour.Properties.copy(Blocks.DEEPSLATE)
+                    .strength(3f).requiresCorrectToolForDrops(), UniformInt.of(10,11)));
+       public static final RegistryObject<Block> NETHER_SAPPHIRE_ORE = registerBlock("nether_sapphire_ore",
+            () -> new DropExperienceBlock( BlockBehaviour.Properties.copy(Blocks.NETHERRACK)
+                    .strength(3f).requiresCorrectToolForDrops(), UniformInt.of(10,11)));
+       public static final RegistryObject<Block> END_STONE_SAPPHIRE_ORE = registerBlock("end_stone_sapphire_ore",
+            () -> new DropExperienceBlock( BlockBehaviour.Properties.copy(Blocks.END_STONE)
+                    .strength(5f).requiresCorrectToolForDrops(), UniformInt.of(10,11)));
+
+       //copiar codigo e mudar os campos
        public static final  RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block",
                () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
 
--- src/main/java/net/ricardo/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/ricardo/tutorialmod/item/ModCreativeModeTabs.java
@@ -16,14 +16,26 @@ public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
 
+    //registar na tab de criativo
     public static final RegistryObject<CreativeModeTab> TURORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
                     .title(Component.translatable("creativetab.tutorial_tab"))
                     .displayItems((pParameters, pOutput) -> {
+
+                        //ITEMS
                         pOutput.accept(ModItems.SAPPHIRE.get());
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+
+
+                        //BLOCKS
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                        //ORES
+                        pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
 
                     })
                     .build());
